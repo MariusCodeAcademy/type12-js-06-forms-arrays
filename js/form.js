847574ecae75547d10b5a685487fdd0ed7ab786c
Marius <marius@example.com>
2022-07-13 15:29:05 +0300

    formAgeValue

--- js/form.js
+++ js/form.js
@@ -4,6 +4,8 @@ console.log('form');
 // 1 nusitaikyt i elementus kuriuos naudosim
 const userFormEl = document.getElementById('userForm');
 console.log('userFormEl ===', userFormEl);
+const usernameEl = document.getElementById('username');
+const ageEl = document.getElementById('age');
 
 // uzdeti formai ivykio submit pasiklausyma
 userFormEl.addEventListener('submit', handleFormSubmit);
@@ -12,8 +14,13 @@ function handleFormSubmit(event) {
   // 2. nusitaike i forma mes sustabdom josissiuntima su restartu
   // event.preventDefault(); sustabdyti numatytaji veiksma
   event.preventDefault();
-
-  console.log('Submit js is in control', event);
+  console.log('Submit js is in control');
+  // ageEl === <input type="number" id="age" name="age" placeholder="Your age">
+  // kas ivesta i ageEl === ageEl.value
+  const formAgeValue = ageEl.value;
+  console.log('formAgeValue ===', formAgeValue);
+  // usernameValue
+  // pagaminti objekta kuriame butu username ir age su reikmem
 }
 
 // 3. pereme formos issiuntimo eventa mes pasiimam ivesetties laukur reiksmes
