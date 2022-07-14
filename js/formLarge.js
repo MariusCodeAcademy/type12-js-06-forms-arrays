47915ed713e2307fc356a2b4609062b7941a8e40
Marius <marius@example.com>
2022-07-14 14:01:50 +0300

    genderPasirinkimas

--- js/formLarge.js
+++ js/formLarge.js
@@ -2,11 +2,24 @@
 console.log('formLarge');
 
 const cardEl = document.getElementById('card');
+const h1El = document.querySelector('h1');
 const veskInputEl = document.getElementById('ivesk');
-
+const maleEl = document.getElementById('male');
+const femaleEl = document.getElementById('female');
+// const formEl = document.getElementById('userForm');
+const formEl = document.forms.radioForm;
+console.log('formEl ===', formEl);
 // veskInputEl.value = 'Hello';
 
 veskInputEl.addEventListener('input', (event) => {
   console.log('input ivyko');
-  cardEl.textContent = event.target.value;
+  h1El.textContent = event.target.value;
+});
+
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Js is in control');
+  const obj = {};
+  const genderPasirinkimas = maleEl.checked ? 'Male' : 'Female';
+  console.log('genderPasirinkimas ===', genderPasirinkimas);
 });
