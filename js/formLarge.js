e62c7727805cfd8cb03681e131483a11a5470438
Marius <marius@example.com>
2022-07-14 15:18:43 +0300

    yes

--- js/formLarge.js
+++ js/formLarge.js
@@ -4,10 +4,12 @@ console.log('formLarge');
 const cardEl = document.getElementById('card');
 const h1El = document.querySelector('h1');
 const veskInputEl = document.getElementById('ivesk');
+const colorBlockEl = document.getElementById('colors');
 const maleEl = document.getElementById('male');
 const femaleEl = document.getElementById('female');
 // const formEl = document.getElementById('userForm');
 const formEl = document.forms.radioForm;
+// const inner = document.body.innerHTML;
 console.log('formEl ===', formEl);
 // veskInputEl.value = 'Hello';
 
@@ -19,7 +21,43 @@ veskInputEl.addEventListener('input', (event) => {
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log('Js is in control');
-  const obj = {};
+
   const genderPasirinkimas = maleEl.checked ? 'Male' : 'Female';
   console.log('genderPasirinkimas ===', genderPasirinkimas);
+  // spalvos
+  const colorsSelectedArrLike = colorBlockEl.querySelectorAll('input:checked');
+  const len = colorsSelectedArrLike.length;
+  const selectedColors = [];
+  for (let i = 0; i < len; i++) {
+    const el = colorsSelectedArrLike[i].value;
+    selectedColors.push(el);
+  }
+  console.log('selectedColors ===', selectedColors);
+
+  const obj = {
+    email: 'email@example.com',
+    pass: 'pass@example.com',
+    gender: genderPasirinkimas,
+    town: 'kaunas',
+    favColor: selectedColors,
+  };
+  console.log('obj ===', obj);
+
+  outputColors(obj.favColor);
 });
+
+function outputColors(arr) {
+  // i textContent paduoti reikia stringa
+  // Array.join(skirtukas) - sujungia array i stringa
+  cardEl.textContent = arr.join(', ');
+  console.log('{ items: arr } ===', { items: arr });
+}
+
+// nusitaikyti i terms input
+// nusitaikyti i submit button
+
+//  terms input uzdeti eventa (change)
+
+// ivykus eventui mes tikrinam ar checked yra true
+// jei yra tai nuimam nuo mygtuko disabled
+// jei ne uzdedam
